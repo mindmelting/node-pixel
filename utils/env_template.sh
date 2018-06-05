21c5988f8b2196757699156b3729c91901cf1c98
Lawrence Hunt <lawrence.hunt@gmail.com>
2018-06-05 22:32:27 +1000

    chore(rgbw): Add init support for rgbw

--- utils/env_template.sh
+++ utils/env_template.sh
@@ -1,7 +1,6 @@
-#! /bin/sh
+#! /bin/bash
 
 # this is a template for setting the environment variables you need for things
 # like building etc so grunt can do what it needs to do if needed.
 
-export ARDUINO_PATH=~/Downloads/arduino.app/Contents/MacOS/Arduino
-
+export ARDUINO_PATH=~/Applications/Arduino.app/Contents/MacOS/Arduino
